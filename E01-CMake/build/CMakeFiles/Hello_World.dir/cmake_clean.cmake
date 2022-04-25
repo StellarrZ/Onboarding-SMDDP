8dc06e7eade63833a77b884a98de62bb7ef5b6ed
Ze Sheng <z7sheng@uwaterloo.ca>
2022-04-25 17:10:50 +0000

    Completed E01-CMake

---
+++ E01-CMake/build/CMakeFiles/Hello_World.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Hello_World.dir/Hello_World.c.o"
+  "Hello_World"
+  "Hello_World.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Hello_World.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
